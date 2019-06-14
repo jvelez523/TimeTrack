4cdee975b7ed9036dedf7b56f7ad70433f75367d
jvelez523 <justin.velez0523@gmail.com>
2019-06-13 22:56:10 -0400

    Invoices page updated with title, button, and card boilerplate

--- src/pages/Dashboard/Invoices.jsx
+++ src/pages/Dashboard/Invoices.jsx
@@ -1,11 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Layout, Button, Typography, Row, Col } from "antd";
+import CardComp from "./components/Card";
+const { Title } = Typography
+const { Content, Header } = Layout;
 
 export default class Invoices extends Component {
   render() {
     return (
-      <div>
-        <p>Invoices</p>
-      </div>
-    )
+      <React.Fragment>
+        <Content style={{ margin: "50px" }}>
+          <Header style={{ background: "#F0F2F5", justifyContent: "space-between", display: "flex", alignItems: "center", padding: "0px" }}>
+            <Title>Invoices</Title>
+            <Button size="large">Add New</Button>
+          </Header>
+          <Content>
+        <Row type='flex' >
+          <CardComp />
+        </Row>
+        </Content>
+        </Content>
+      </React.Fragment>
+    );
   }
 }
---
+++ src/pages/Dashboard/components/Card.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import { Card, Icon, Avatar } from "antd";
+
+const { Meta } = Card;
+
+export default class CardComp extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Card
+          style={{ width: 300, boxShadow: "0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)" }}
+          cover={
+            <img
+              alt="example"
+              src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+            />
+          }
+          actions={[
+            <Icon type="setting" />,
+            <Icon type="edit" />,
+            <Icon type="ellipsis" />
+          ]}
+        >
+          <Meta
+            avatar={
+              <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+            }
+            title="Card title"
+            description="This is the description"
+          />
+        </Card>
+      </React.Fragment>
+    );
+  }
+}
