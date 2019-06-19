750dcd839f28295fe06faad99680789a85b67c26
jvelez523 <justin.velez0523@gmail.com>
2019-06-18 21:42:22 -0400

    Added title to main dash page

--- src/pages/Dashboard/Dashpage.jsx
+++ src/pages/Dashboard/Dashpage.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
-import { Layout } from 'antd';
+import { Layout, Button, Typography } from 'antd';
 import RevenueDisplay from './components/RevenueDisplay';
-const { Content } = Layout;
+const { Title } = Typography
+const { Content, Header } = Layout;
 
 
 export default class Dashpage extends Component {
@@ -9,6 +10,9 @@ export default class Dashpage extends Component {
     return (
       <React.Fragment>
       <Content style={{ margin: '50px' }}>
+      <Header style={{ background: "#F0F2F5", justifyContent: "space-between", display: "flex", alignItems: "center", padding: "0px" }}>
+            <Title>Dashboard</Title>
+          </Header>
         <RevenueDisplay />
         </Content>
       </React.Fragment>
