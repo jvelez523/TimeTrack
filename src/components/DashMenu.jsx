ae79c9527d31b21041279b4d4344f86f528bee5e
jvelez523 <justin.velez0523@gmail.com>
2019-06-21 23:44:31 -0400

    Removed commented out code

--- src/components/DashMenu.jsx
+++ src/components/DashMenu.jsx
@@ -47,17 +47,6 @@ class Dash extends React.Component {
             </Menu.Item>
           </Menu>
         </Sider>
-        {/* <Layout>
-          <Header style={{ background: '#fff', padding: 0 }} />
-          <Content style={{ margin: '0 16px' }}>
-            <Breadcrumb style={{ margin: '16px 0' }}>
-              <Breadcrumb.Item>User</Breadcrumb.Item>
-              <Breadcrumb.Item>Bill</Breadcrumb.Item>
-            </Breadcrumb>
-            <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>Bill is a cat.</div>
-          </Content>
-          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
-        </Layout> */}
         <Layout>
         <Route exact path="/dashboard" component={Dashpage} />
         <Route exact path="/dashboard/invoices" component={Invoices} />
