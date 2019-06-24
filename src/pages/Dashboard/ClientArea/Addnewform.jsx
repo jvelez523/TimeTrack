c890f640caaf2d99fd8d09eabf74551eae4b8a1e
jvelez523 <justin.velez0523@gmail.com>
2019-06-24 00:04:26 -0400

    Added modal for clients form with loading status on submit

---
+++ src/pages/Dashboard/ClientArea/Addnewform.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react'
+import { Modal} from 'antd'
+
+export default class Addnewform extends Component {
+
+  state = {
+    ModalText: 'Content of the modal',
+    visible: false,
+    confirmLoading: false,
+  };
+
+
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+
+  handleOk = () => {
+    this.setState({
+      ModalText: 'The modal will be closed after two seconds',
+      confirmLoading: true,
+    });
+    setTimeout(() => {
+      this.setState({
+        visible: false,
+        confirmLoading: false,
+      });
+    }, 2000);
+  };
+
+
+  handleCancel = () => {
+    console.log('Clicked cancel button');
+    this.setState({
+      visible: false,
+    });
+  };
+
+
+  render() {
+
+    const { visible, confirmLoading, ModalText } = this.state;
+
+
+
+
+    return (
+      <div>
+        <Modal
+          title="Title"
+          visible={this.props.visible}
+          onOk={this.props.handleOK}
+          confirmLoading={this.props.loading}
+          onCancel={this.props.cancel}
+        >
+          <p>{ModalText}</p>
+        </Modal>
+      </div>
+    )
+  }
+}
--- src/pages/Dashboard/Clients.jsx
+++ src/pages/Dashboard/Clients.jsx
@@ -1,23 +1,59 @@
 import React, { Component } from 'react'
 import { Layout, Button, Typography, Row, Col } from "antd";
 import CardComp from "./components/Card";
+import Addnewform from './ClientArea/Addnewform';
 const { Title } = Typography
 const { Content, Header } = Layout;
 
 export default class Clients extends Component {
+
+  state = {
+    ModalText: 'Content of the modal',
+    visible: false,
+    confirmLoading: false,
+  };
+
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  handleOk = () => {
+    this.setState({
+      ModalText: 'The modal will be closed after two seconds',
+      confirmLoading: true,
+    });
+    setTimeout(() => {
+      this.setState({
+        visible: false,
+        confirmLoading: false,
+      });
+    }, 2000);
+  };
+
+  handleCancel = () => {
+    console.log('Clicked cancel button');
+    this.setState({
+      visible: false,
+    });
+  };
+
+
   render() {
     return (
       <React.Fragment>
         <Content style={{ margin: "50px" }}>
           <Header style={{ background: "#F0F2F5", justifyContent: "space-between", display: "flex", alignItems: "center", padding: "0px" }}>
             <Title>Clients</Title>
-            <Button size="large">Add New</Button>
+            <Button size="large" onClick={this.showModal}>Add New</Button>
           </Header>
           <Content>
         <Row type='flex' >
           <CardComp />
         </Row>
         </Content>
+        <Addnewform visible={this.state.visible} cancel={this.handleCancel} handleOK={this.handleOk} loading={this.state.confirmLoading}/>
         </Content>
       </React.Fragment>
     )
